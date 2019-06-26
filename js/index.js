8b29128b5b06982226c11ac2024c126fca2effee
Zach Lasky <zacharylasky123@gmail.com>
2019-06-26 12:44:12 -0400

    added moving picture gallery to mentor and mentee

--- js/index.js
+++ js/index.js
@@ -30,3 +30,46 @@ aLinks.forEach(link => {
         link.style.backgroundColor = 'white';
     })
 })
+
+//MOVING GALLERY FOR MENTOR / MENTEE FUNCTION
+let i = 0; //start
+let images = [];
+let time = 3000;
+//images
+images[0] = "./img/work.jpeg";
+images[1] = "./img/business.jpeg" ;
+images[2] = "./img/at-work.jpg" ;
+
+//change image
+function changeImg() {
+	document.slide1.src = images[i];
+	if (i < images.length - 1) {
+		i++;
+	}
+	else {
+		i = 0;
+	}
+	setTimeout("changeImg()", time);
+}
+window.onload = changeImg();
+
+let j = 0; //start
+let images2 = [];
+let time2 = 3000;
+//images
+images2[0] = "./img/entrepreneur.jpg";
+images2[1] = "./img/charts.jpg";
+images2[2] = "./img/mentee.jpeg";
+
+//change image
+function changeImg2() {
+	document.slide2.src = images2[j];
+	if (j < images2.length - 1) {
+		j++;
+	}
+	else {
+		j = 0;
+	}
+	setTimeout("changeImg2()", time2);
+}
+window.onload = changeImg2();
